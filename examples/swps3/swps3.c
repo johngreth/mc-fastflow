336c056d452da2aea38e314af315a30376301ad2
Hongyi Xin <hxin@sloth.ece.cmu.edu>
2014-04-18 16:51:30 -0400

    New stuff.

--- examples/swps3/swps3.c
+++ examples/swps3/swps3.c
@@ -35,9 +35,7 @@
 #include "DynProgr_sse_short.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/select.h>
+#include <sys/times.h>
 #include <unistd.h>
 #include <string.h>
 #include <assert.h>
@@ -50,73 +48,117 @@
 #define BATCH_RUN 1
 
 int main( int argc, char * argv[] ){
-    int queryLen, dbLen;
+    int queryLen = 0;
+	int dbLen = 0;
     char query[200];
     char db[200];
-    Options options = {-1, -1,DBL_MAX};
+    Options options = {-1, -1, DBL_MAX};
     int qCount=0, dCount=0, qResidues=0, dResidues=0;
     SBMatrix matrix;
-    int max_score = atoi( argv[1]);
+    int max_error = atoi( argv[1]);
+	
     matrix   = swps3_readSBMatrix( NULL );
-    char read_strs[BATCH_RUN][200];
-    char ref_strs[BATCH_RUN][200];
+
+    char** read_strs;
+    char** ref_strs;
+
+	read_strs = (char**) malloc(BATCH_RUN * sizeof(char*) );
+	ref_strs = (char**) malloc(BATCH_RUN * sizeof(char*) );
+	
+	int i;
+	for (i = 0; i < BATCH_RUN; i++) {
+		read_strs[i] = (char*) malloc(200 * sizeof(char) );
+		ref_strs[i] = (char*) malloc(200 * sizeof(char) );
+	}
 
     if (argc != 2) {
         printf("$>bin error\n");
         exit(1);
     }
-    long long align_num = 0;
-    long long total_num = 0;
 
     long long read_size;
     long long read_idx;
 
+	struct tms start_time;
+	struct tms end_time;
+	struct tms elp_time;
 
-    clock_t start;
-    long long unsigned total;
+	elp_time.tms_stime = 0;
+	elp_time.tms_utime = 0;
+	elp_time.tms_cstime = 0;
+	elp_time.tms_cutime = 0;
+
+    long long unsigned totalNum = 0;
+	long long unsigned passNum = 0;
 
     int stop = 0;
-    while (!stop) {  
+	do {
         stop = 0;
-        for (read_size = 0; read_size < BATCH_RUN; read_size++) {		
+        for (read_size = 0; read_size < BATCH_RUN; read_size++) {
             fgets(read_strs[read_size], 200, stdin);
             if (strcmp(read_strs[read_size], "end_of_file\n") == 0) {
                 stop = 1;
                 break;
             }
+			int temp_length = strlen(read_strs[read_size]) - 1;
+			if (temp_length > queryLen) {
+				queryLen = temp_length;
+				dbLen = temp_length;
+				printf("%d\n", queryLen);
+			}
+			read_strs[queryLen] = '\0';
 
             fgets(ref_strs[read_size], 200, stdin);
+			ref_strs[queryLen] = '\0';
         }
 
-        start = clock();
+        times(&start_time);
 
         for (read_idx = 0; read_idx < read_size; read_idx++) {
-            queryLen = strlen(read_strs[read_idx]) - 1;
             memcpy(query, read_strs[read_idx], queryLen);
-            swps3_translateSequence(query, queryLen, NULL);
-            double score = 0;
-            ProfileByte  * profileByte = swps3_createProfileByteSSE( query, queryLen, matrix );
-            qCount++; qResidues+=queryLen;
-            dCount=dResidues=0;
-            dbLen = strlen(ref_strs[read_idx]) - 1;
-            memcpy(db, ref_strs[read_idx], dbLen);;
-            swps3_translateSequence(db, dbLen, NULL);
+            swps3_translateSequence(read_strs[read_idx], queryLen, NULL);
+            unsigned score = 0;
+            ProfileByte  * profileByte = swps3_createProfileByteSSE( read_strs[read_idx], queryLen, matrix );
+            qCount++;
+			qResidues += queryLen;
+            dCount = dResidues = 0;
+            memcpy(db, ref_strs[read_idx], dbLen);
+            swps3_translateSequence(ref_strs[read_idx], dbLen, NULL);
 
-            score = queryLen - swps3_alignmentByteSSE( profileByte, db, dbLen, &options); 
-            if(score <= max_score) { 
+            score = queryLen - swps3_alignmentByteSSE( profileByte, ref_strs[read_idx], dbLen, &options); 
+            if(score <= max_error) { 
                 fprintf(stderr, "%d\n", (int)score);
-                align_num++;
-                fprintf(stderr, "%s", read_strs[read_idx] );
-                fprintf(stderr, "%s", ref_strs[read_idx] );
+                passNum++;
+                fprintf(stderr, "%s\n", read_strs[read_idx] );
+                fprintf(stderr, "%s\n", ref_strs[read_idx] );
             }
 
             dCount++; dResidues+=dbLen;
 
-            total_num++;
+            totalNum++;
         }
-        total += clock() - start;
-    }
-    fprintf(stderr,"%d[%d] x %d[%d]\n", qCount, qResidues, dCount, dResidues );
+
+		times(&end_time);
+		elp_time.tms_stime += end_time.tms_stime - start_time.tms_stime;
+		elp_time.tms_utime += end_time.tms_utime - start_time.tms_utime;
+		
+		if (stop)
+			break;
+
+    } while (!stop);
+	
+	fprintf(stderr, "end_of_file\n");
+	printf("passNum:\t%lld\n", passNum);
+	printf("totalNum:\t%lld\n", totalNum);
+	printf("total_time: %f\n", (double) elp_time.tms_utime / sysconf(_SC_CLK_TCK) ); 
+
+	for (i = 0; i < BATCH_RUN; i++) {
+		free(read_strs[i]);
+		free(ref_strs[i]);
+	}
+	free(read_strs);
+	free(ref_strs);
+
 
     return 0;
 }
